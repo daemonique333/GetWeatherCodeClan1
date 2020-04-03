6c50ca87c464a1d8cbda8a5b483491dada39f901
daemonique333 <Daemonique333@yahoo.com>
2020-04-03 14:38:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,16 @@
+function getWeather (country, weatherType){
+
+
+   return "The weather in " + country + " is " + weatherType;
+};
+
+
+
+  
+
+  
+  
+  console.log(getWeather("Scotland", "sunny"));
+  console.log(getWeather("Hungary", "cloudy"));
+  console.log(getWeather("England", "warm"));
+
